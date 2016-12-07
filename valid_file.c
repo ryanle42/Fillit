b71c7e7fffebd3e788e21dc84e60f604fef75134
Ryan Le <rleinbox@gmail.com>
2016-12-07 14:38:45 -0800

    file validation

---
+++ valid_file.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int     valid_file(char *file)
+{
+    int lcount;
+    int nlcount;
+    int bcount;
+    int i;
+
+    i = 0;
+    if (!file[i])
+        return (0);    
+    while (file[i])
+    {
+        nlcount = 0;
+        bcount = 0;
+        while (nlcount < 4)
+        {
+            lcount = 0;
+            while (lcount < 4)
+            {
+                if (file[i] == '.' || file[i] == '#')
+                {
+                    if (file[i] == '#')
+                        bcount++;
+                    i++;
+                    lcount++;
+                }
+                else
+                    return (0);
+            }
+            if (file[i] == '\n')
+            {
+                i++;
+                nlcount++;
+            }
+            else
+                return (0);
+        }
+        if (bcount != 4)
+            return (0);
+        if (file[i] == '\n')
+            i++;
+        else
+            return (0);
+    }
+    return (1);
+}
+
+int main()
+{
+    char *file = "...#\n.#.#\n....\n.#..\n\n...#\n....\n..#.\n.##.\n\n";
+    printf("%i\n",valid_file(file));
+    return (1);
+}
